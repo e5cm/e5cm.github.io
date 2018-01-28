ea9bc174567b1a0fcf5f87d8bab68ba0df5acbce
yinzhennan <yinzhennan123@gmail.com>
2018-01-28 22:55:43 +0800

    slide

--- js/e5Show.js
+++ js/e5Show.js
@@ -1,5 +1,8 @@
 var app = angular.module('songApp', []);
 app.controller('songCtrl', function($scope, $timeout) {
+    $scope.switchRc = function(data) {
+        return [data[0],data[2],data[4],data[6],data[8],data[10],data[1],data[3],data[5],data[7],data[9],data[11]];
+    };
     $scope.songs = songData;
     $scope.slide = "mid";
     $scope.typeEnum = e5.typeEnum;
@@ -7,6 +10,9 @@ app.controller('songCtrl', function($scope, $timeout) {
     $scope.songsToShow=$scope.songs.slice(0,10);
     $scope.songsPreLoad=[];
     $scope.songsToShow.unshift({},{});
+    $scope.songsToShow = $scope.switchRc($scope.songsToShow);
+
+
 
     //TODO 统一两个方法,改为过滤器
     $scope.slideNext = function(){
@@ -26,28 +32,50 @@ app.controller('songCtrl', function($scope, $timeout) {
         }, 190);
     };
     $scope.slideTo =  function(showIndex){
+        var filterSongs = $scope.songs.concat([]);
+        if($scope.codeFilter){
+            filterSongs=$scope.filterByE5Code(filterSongs,$scope.codeFilter);
+        }
         //跳转结尾
         if(showIndex<0){
-            showIndex = Math.ceil(($scope.songs.length - 8)/2)*2;
+            showIndex = Math.ceil((filterSongs.length - 8)/2)*2;
         }
         //跳转开头
-        if(showIndex > $scope.songs.length - 6){
+        if(showIndex > filterSongs.length - 6){
             showIndex = 0;
         }
         //修正
         $scope.showIndex = showIndex;
         var tmp;
         if(showIndex == 0){
-            tmp = $scope.songs.slice(showIndex,showIndex + 10);
+            tmp = filterSongs.slice(showIndex,showIndex + 10);
             tmp.unshift({},{});
         }else{
-            tmp = $scope.songs.slice(showIndex-2,showIndex + 10);
+            tmp = filterSongs.slice(showIndex-2,showIndex + 10);
         }
         while(tmp.length<12){
             tmp.push({});
         }
-        $scope.songsToShow = tmp;
+        $scope.songsToShow = $scope.switchRc(tmp);
+
     };
+    $scope.codeFilterChange = function () {
+        $scope.slideTo(0);
+    };
+
+    $scope.filterByE5Code = function (songs, code) {
+        var result = [];
+        for(var song in songs){
+            song = songs[song];
+            if(song.e5code && angular.lowercase(song.e5code).indexOf(angular.lowercase(code))>-1){
+                result.push(song);
+            }else if(song.e5code == "random"){
+                result.push(song);
+            }
+        }
+        return result;
+    };
+
 
     //预加载
     // $timeout(function () {
@@ -55,14 +83,15 @@ app.controller('songCtrl', function($scope, $timeout) {
     // },2000);
 });
 
-app.filter('typeName', function() { //可以注入依赖
+app.filter('typeName', function() {
     return function(text) {
         return e5.typeEnum[text];
     }
 });
-
-app.filter('switchRc', function() { //可以注入依赖
-    return function(data) {
-        return [data[0],data[2],data[4],data[6],data[8],data[10],data[1],data[3],data[5],data[7],data[9],data[11]];
+app.filter('notEmpty', function() {
+    return function(text) {
+        if(text){
+            return "not-empty";
+        }
     }
 });
